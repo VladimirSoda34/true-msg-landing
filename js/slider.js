b9c4d5662a3e5d17ac47aed9bfc47bf457f25b8f
novoexpert <novoexpert@gmail.com>
2023-02-24 18:12:27 +0300

    slider

---
+++ js/slider.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // активация слайдера
+    new ItcSimpleSlider('.itcss', {
+      loop: true,
+      autoplay: false,
+      swipe: true
+    });
+  });
